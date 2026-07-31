56fc10ccefe92dc354479bc061a13c447c6a368f
Raj Singh <rajsinghcpre@gmail.com>
2026-07-31 16:56:24 -0500

    bhaiya 0.1.3

---
+++ Casks/bhaiya.rb
@@ -0,0 +1,31 @@
+cask "bhaiya" do
+  version "0.1.3"
+  sha256 "df15fb114a6b818ff030afffd9849735551370d827994d82c232e2a169efd742"
+
+  url "https://github.com/rajsinghtech/homebrew-tap/releases/download/bhaiya-v#{version}/Bhaiya-#{version}-macOS.zip"
+  name "Bhaiya Client"
+  desc "Native macOS client for Bhaiya workstations"
+  homepage "https://github.com/rajsinghtech/homebrew-tap"
+
+  livecheck do
+    url "https://api.github.com/repos/rajsinghtech/homebrew-tap/releases"
+    regex(/^bhaiya-v?(\d+(?:\.\d+)+)$/i)
+    strategy :json do |json, regex|
+      json.map { |release| release["tag_name"]&.match(regex)&.captures&.first }
+    end
+  end
+
+  app "Bhaiya Client.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/Bhaiya Client.app"],
+                   sudo: false
+  end
+
+  zap trash: [
+    "~/Library/Application Support/top.keiretsu.bhaiya.client",
+    "~/Library/Caches/top.keiretsu.bhaiya.client",
+    "~/Library/Preferences/top.keiretsu.bhaiya.client.plist",
+  ]
+end
